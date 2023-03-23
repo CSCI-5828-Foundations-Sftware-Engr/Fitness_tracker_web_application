4346ae0cad9a9710a833e790162c0b206b1e4975
matsy <sai.mekala@colorado.edu>
2023-03-22 20:38:55 -0600

    added login

--- server.py
+++ server.py
@@ -1,17 +1,18 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-from pymongo import MongoClient
-import hashlib
+import hashlib, pymongo
 
 app = Flask(__name__)
+app.secret_key = "testing"
 CORS(app)
 
 # connect to MongoDB
-client = MongoClient()
-db = client['data_base_name']
-collection = client["user_info"]
+client = pymongo.MongoClient("mongodb://127.0.0.1:27017")
+db = client['fitness_tracker']
+collection = client["user_login_info"]
 
-@app.route('/register', methods=['POST'])
+@app.route('/', methods=['post','get'])
+@app.route('/login', methods=['post','get'])
 def register():
     # get the request data
     data = request.get_json()
@@ -28,5 +29,5 @@ def register():
     # return the user ID
     return jsonify({'user_id': str(result.inserted_id)})
 
-if __name__ == '_main_':
-    app.run()
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", debug = True)
