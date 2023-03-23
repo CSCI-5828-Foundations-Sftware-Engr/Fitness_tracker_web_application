65f30caabc19e193cc6d77a09784922699aef3df
Sai Akhil <akhiltejamekala@gmail.com>
2023-03-23 02:12:02 -0600

    HW5 final working

--- app.py
+++ app.py
@@ -24,23 +24,3 @@ def register():
         return send_from_directory(app.static_folder,'index.html')
 
 api.add_resource(HelloApiHandler, '/flask/hello')
-
-# connect to MongoDB
-# client = pymongo.MongoClient("mongodb://127.0.0.1:27017")
-# db = client['fitness_tracker']
-# collection = client["user_login_info"]
-
-# @app.route('/', methods=['post','get'])
-# @app.route('/login', methods=['post','get'])
-# def register():
-#     # get the request data
-#     print(request)
-#     data = request.get_json()
-#     print(json.dumps(data, indent=4))
-#     # res = {"data" : {"calorie_intake": data['calorie_intake'], "calorie_burnt": data['calorie_burnt']}}
-#     res ={"calorie_intake": data['calorie_intake'], "calorie_burnt": data['calorie_burnt']}
-#     print(res)
-#     return res
-
-# if __name__ == '__main__':
-#     app.run(host="0.0.0.0", debug = True)
