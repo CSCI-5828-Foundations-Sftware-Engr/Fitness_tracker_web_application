a349b19a3f772be4996b5743552735eabc3a5aaa
Manali Kale <manaliakale@gmail.com>
2023-04-07 19:23:12 -0600

    Updated CSS

--- fitness-tracker-react/src/App.js
+++ fitness-tracker-react/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
-import React, { useState } from "react";
-import logo from "./logo-transparent-svg.svg";
+import LoginForm from './LoginForm';
+
 
 function App() {
   return (
@@ -9,90 +9,5 @@ function App() {
     </div>
   );
 }
-
-const LoginForm = props => (
-  <div id="loginform">
-    <FormHeader title="Fitness-Tracker-Application" />
-    <Form />
-  </div>
-);
-
-const FormHeader = props => {
-  const [activeTab, setActiveTab] = useState('login');
-
-  const handleTabChange = tab => {
-    setActiveTab(tab);
-  };
-
-  return (
-    <div >
-      <div class="form-header">
-      <img src={logo} width={200} height={200} />
-      </div>   
-      <div className="tabs">
-        <button
-          className={activeTab === 'login' ? 'active' : ''}
-          onClick={() => handleTabChange('login')}
-        >
-          Login
-        </button>
-        <button
-          className={activeTab === 'signup' ? 'active' : ''}
-          onClick={() => handleTabChange('signup')}
-        >
-          Signup
-        </button>
-      </div>
-    </div>
-  );
-};
-
-
-
-const Form = props => (
-  <div>
-    <FormInput description="Username" placeholder="Enter Username" type="text" />
-    <FormInput description="Password" placeholder="Enter Password" type="password" />
-    <FormButton title="Submit" />
-  </div>
-);
-
-const FormButton = props => {
-  const [response, setResponse] = React.useState('');
-  const postData = () => {
-    
-    const username = document.querySelector('input[type="text"]').value;
-    const password = document.querySelector('input[type="password"]').value;
-
-    fetch('/login', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        username: username,
-        password: password
-      })
-    })
-    .then(response => response.json())
-    .then(data => {console.log(data); setResponse(data)})
-    .catch(error => console.error(error));
-  }
-
-  return (
-    <div>
-      <div id="button" class="row" onClick={postData}>
-        <button>{props.title}</button>
-      </div>
-    </div>
-  );
-}
-
-const FormInput = props => (
-  <div class="row">
-    <label>{props.description}</label>
-    <input type={props.type} placeholder={props.placeholder} />
-  </div>
-);
 
 export default App;
