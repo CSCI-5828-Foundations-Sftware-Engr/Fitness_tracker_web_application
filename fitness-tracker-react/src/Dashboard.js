731a41882712c2734d771a1fadb9ff96837953ca
Manali Kale <manaliakale@gmail.com>
2023-04-29 19:11:28 -0600

    Added Dashboard

---
+++ fitness-tracker-react/src/Dashboard.js
@@ -0,0 +1,14 @@
+import Header from "./Header";
+import SideMenu from "./SideMenu";
+
+const Dashboard = () => {
+    return (
+        <div class="dashboard">
+            <Header />
+            <SideMenu />
+        </div>
+        
+    );
+}
+
+export default Dashboard;
